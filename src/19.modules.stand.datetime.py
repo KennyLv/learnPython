0b5e252c619dbe77d403b7d3805d8350af40f626
Qing, Lv <luqin@airbiquity.com>
2016-04-25 15:55:06 +0800

    add demo for python

---
+++ src/19.modules.stand.datetime.py
@@ -0,0 +1,177 @@
+﻿#!/usr/bin/env python
+# coding: UTF-8
+
+print('Hello World!')
+
+import time, datetime
+'''
+    asctime(...)
+        asctime([tuple]) -> string
+        
+        Convert a time tuple to a string, e.g. 'Sat Jun 06 16:26:11 1998'.
+        When the time tuple is not present, current time as returned by localtime()
+        is used.
+    
+    clock(...)
+        clock() -> floating point number
+        
+        Return the CPU time or real time since the start of the process or since
+        the first call to clock().  This has as much precision as the system
+        records.
+    
+    ctime(...)
+        ctime(seconds) -> string
+        
+        Convert a time in seconds since the Epoch to a string in local time.
+        This is equivalent to asctime(localtime(seconds)). When the time tuple is
+        not present, current time as returned by localtime() is used.
+    
+    get_clock_info(...)
+        get_clock_info(name: str) -> dict
+        
+        Get information of the specified clock.
+    
+    gmtime(...)
+        gmtime([seconds]) -> (tm_year, tm_mon, tm_mday, tm_hour, tm_min,
+                               tm_sec, tm_wday, tm_yday, tm_isdst)
+        
+        Convert seconds since the Epoch to a time tuple expressing UTC (a.k.a.
+        GMT).  When 'seconds' is not passed in, convert the current time instead.
+        
+        If the platform supports the tm_gmtoff and tm_zone, they are available as
+        attributes only.
+    
+    localtime(...)
+        localtime([seconds]) -> (tm_year,tm_mon,tm_mday,tm_hour,tm_min,
+                                  tm_sec,tm_wday,tm_yday,tm_isdst)
+        
+        Convert seconds since the Epoch to a time tuple expressing local time.
+        When 'seconds' is not passed in, convert the current time instead.
+    
+    mktime(...)
+        mktime(tuple) -> floating point number
+        
+        Convert a time tuple in local time to seconds since the Epoch.
+        Note that mktime(gmtime(0)) will not generally return zero for most
+        time zones; instead the returned value will either be equal to that
+        of the timezone or altzone attributes on the time module.
+    
+    monotonic(...)
+        monotonic() -> float
+        
+        Monotonic clock, cannot go backward.
+    
+    perf_counter(...)
+        perf_counter() -> float
+        
+        Performance counter for benchmarking.
+    
+    process_time(...)
+        process_time() -> float
+        
+        Process time for profiling: sum of the kernel and user-space CPU time.
+    
+    sleep(...)
+        sleep(seconds)
+        
+        Delay execution for a given number of seconds.  The argument may be
+        a floating point number for subsecond precision.
+    
+    strftime(...)
+        strftime(format[, tuple]) -> string
+        
+        Convert a time tuple to a string according to a format specification.
+        See the library reference manual for formatting codes. When the time tuple
+        is not present, current time as returned by localtime() is used.
+        
+        Commonly used format codes:
+        
+        %Y  Year with century as a decimal number.
+        %m  Month as a decimal number [01,12].
+        %d  Day of the month as a decimal number [01,31].
+        %H  Hour (24-hour clock) as a decimal number [00,23].
+        %M  Minute as a decimal number [00,59].
+        %S  Second as a decimal number [00,61].
+        %z  Time zone offset from UTC.
+        %a  Locale's abbreviated weekday name.
+        %A  Locale's full weekday name.
+        %b  Locale's abbreviated month name.
+        %B  Locale's full month name.
+        %c  Locale's appropriate date and time representation.
+        %I  Hour (12-hour clock) as a decimal number [01,12].
+        %p  Locale's equivalent of either AM or PM.
+        
+        Other codes may be available on your platform.  See documentation for
+        the C library strftime function.
+    
+    strptime(...)
+        strptime(string, format) -> struct_time
+        
+        Parse a string to a time tuple according to a format specification.
+        See the library reference manual for formatting codes (same as
+        strftime()).
+        
+        Commonly used format codes:
+        
+        %Y  Year with century as a decimal number.
+        %m  Month as a decimal number [01,12].
+        %d  Day of the month as a decimal number [01,31].
+        %H  Hour (24-hour clock) as a decimal number [00,23].
+        %M  Minute as a decimal number [00,59].
+        %S  Second as a decimal number [00,61].
+        %z  Time zone offset from UTC.
+        %a  Locale's abbreviated weekday name.
+        %A  Locale's full weekday name.
+        %b  Locale's abbreviated month name.
+        %B  Locale's full month name.
+        %c  Locale's appropriate date and time representation.
+        %I  Hour (12-hour clock) as a decimal number [01,12].
+        %p  Locale's equivalent of either AM or PM.
+        
+        Other codes may be available on your platform.  See documentation for
+        the C library strftime function.
+    
+    time(...)
+        time() -> floating point number
+        
+        Return the current time in seconds since the Epoch.
+        Fractions of a second may be present if the system clock provides them.
+'''
+
+print(time.time())   # wall clock time, unit: second
+print(time.clock())  # processor clock time, unit: second
+
+print('start')
+time.sleep(2)     # sleep for 10 seconds
+print('wake up')
+
+#time包还定义了struct_time对象。
+#该对象实际上是将挂钟时间转换为年、月、日、时、分、秒……等日期信息，存储在该对象的各个属性中(tm_year, tm_mon, tm_mday...)。
+#下面方法可以将挂钟时间转换为struct_time对象:
+print(time.gmtime())      # 返回struct_time格式的UTC时间
+print(time.localtime())   # 返回struct_time格式的当地时间, 当地时区根据系统环境决定。
+print(time.mktime(time.gmtime()))    # 将struct_time格式转换成wall clock time
+
+t      = datetime.datetime(2012,9,3,21,30)
+t_next = datetime.datetime(2012,9,5,23,30)
+delta1 = datetime.timedelta(seconds = 600)
+delta2 = datetime.timedelta(weeks = 3)
+print(t + delta1)
+print(t + delta2)
+print(t_next - t)
+print(t > t_next)
+
+
+format = "output-%Y-%m-%d-%H%M%S.txt" 
+str    = "output-1997-12-23-030000.txt" 
+t_format      = datetime.datetime.strptime(str, format)  #from datetime import datetime
+#t_format      = t_next.strptime(str, format)
+print(t_format)
+print(t_next.strftime(format))
+
+input('Please enter a code to quit:')
+
+
+
+ 
+ 
---
+++ src/20.modules.stand.os.py
@@ -0,0 +1,99 @@
+﻿#!/usr/bin/env python
+# coding: UTF-8
+
+print('Hello World!')
+
+import os
+
+path = 'D:\\HBuild_WorkSpace\\learnPython\\src\\testdata.txt'
+
+print('basename : ' , os.path.basename(path))    # 查询路径中包含的文件名
+print('dirname     : ' , os.path.dirname(path))     # 查询路径中包含的目录
+print('split path     : ' , os.path.split(path))   # 将路径分割成文件名和目录两个部分，放在一个表中返回
+print(os.path.exists(path))    # 查询文件是否存在
+print(os.path.getsize(path))   # 查询文件大小
+print(os.path.getatime(path))  # 查询文件上一次读取的时间
+print(os.path.getmtime(path))  # 查询文件上一次修改的时间
+print(os.path.isfile(path))    # 路径是否指向常规文件
+print(os.path.isdir(path))     # 路径是否指向目录文件
+
+path2 = os.path.join( 'D:', '\\', 'HBuild_WorkSpace', 'learnPython', 'src', 'testdata.txt')  # 使用目录名和文件名构成一个路径字符串
+print('-----', path2);
+
+p_list = [path, path2]
+print(os.path.commonprefix(p_list))    # 查询多个路径的共同部分
+
+'''
+os包包括各种各样的函数，以实现操作系统的许多功能。这个包非常庞杂。os包的一些命令就是用于文件管理。我们这里列出最常用的:
+
+mkdir(path)
+
+创建新目录，path为一个字符串，表示新目录的路径。相当于$mkdir命令
+
+rmdir(path)
+
+删除空的目录，path为一个字符串，表示想要删除的目录的路径。相当于$rmdir命令
+
+listdir(path)
+
+返回目录中所有文件。相当于$ls命令。
+
+ 
+
+remove(path)
+
+删除path指向的文件。
+
+rename(src, dst)
+
+重命名文件，src和dst为两个路径，分别表示重命名之前和之后的路径。 
+
+ 
+
+chmod(path, mode)
+
+改变path指向的文件的权限。相当于$chmod命令。
+
+chown(path, uid, gid)
+
+改变path所指向文件的拥有者和拥有组。相当于$chown命令。
+
+stat(path)
+
+查看path所指向文件的附加信息，相当于$ls -l命令。
+
+symlink(src, dst)
+
+为文件dst创建软链接，src为软链接文件的路径。相当于$ln -s命令。
+
+ 
+
+getcwd()
+
+查询当前工作路径 (cwd, current working directory)，相当于$pwd命令。
+'''
+
+dirPath = os.path.dirname(path)
+os.mkdir(dirPath + "\\new_dir")
+
+'''
+glob包最常用的方法只有一个, glob.glob()。
+该方法的功能与Linux中的ls相似(参看Linux文件管理命令)，接受一个Linux式的文件名格式表达式(filename pattern expression)，列出所有符合该表达式的文件（与正则表达式类似），将所有文件名放在一个表中返回。
+所以glob.glob()是一个查询目录下文件的好方法。
+
+该文件名表达式的语法与Python自身的正则表达式不同 (你可以同时看一下fnmatch包，它的功能是检测一个文件名是否符合Linux的文件名格式表达式)。 
+'''
+import  glob
+print(glob.glob('D:\\HBuild_WorkSpace\\learnPython\\src\\*'))
+
+import shutil
+shutil.copy(path, 'D:\\HBuild_WorkSpace\\learnPython\\src\\testdata_copy.txt')
+
+
+
+input('Please enter a code to quit:')
+
+
+
+ 
+ 
